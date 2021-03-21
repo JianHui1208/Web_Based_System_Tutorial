4720e93715409e688ac0a5e26c3b508188448ea0
JianHui1208 <jianhuiwilliam80@gmail.com>
2021-03-21 21:00:52 +0800

    change CSS

--- resources/views/showProduct.blade.php
+++ resources/views/showProduct.blade.php
@@ -1,42 +1,37 @@
 @extends('layouts.app')
 @section('content')
-<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>	
-<div class="container">
-	    <div class="row">
-		    <table class="table table-hover table-striped">
-		        <thead>
-		        <tr class="thead-dark">
-		            <th>ID</th>
-		            <th>Image</th>
-		            <th>Name</th>
-		            <th>Category</th>
-                    <th>Quantity</th>
-                    <th>Price</th>
-                    <th>Action</th>
-		        </tr>
-		        </thead>
-		        <tbody>
-		        @foreach($products as $product)
-		            <tr>
-                    <!-- foreach的$products和ProductController的$products是一样的 -->
-                    <!-- foreach的$product和这个里面的$product是一样的 -->
-		                <td>{{$product->id}}</td>
-                        <td><img src="{{ asset('images/')}}/{{$product->image}}" alt="" height="30%"></td>
-		                <td style="max-width:300px">
-		                    <h6>{{$product->name}}</h6>
-		                    <em class="text-muted">{{$product->description}}</em>
-		                </td>
-		                <td>{{$product->catname}}</td>
-                        <td>{{$product->quantity}}</td>
-                        <td>RM {{$product->price}}</td>
-		                <td>
-		                    <a href="{{ route('edit.product', ['id' => $product->id]) }}" class="btn btn-warning"><i class="fas fa-edit"></i>Edit</a>
-		                    <a href="{{ route('delete.product', ['id' => $product->id]) }}" class="btn btn-danger" onclick="return confirm('Sure Want Delete?')"><i class="fas fa-trash"></i>Delete</a>
-		                </td>
-		            </tr>
-                @endforeach
-		        </tbody>
-		    </table>
-	</div>
-</div>
+<table class="table table-hover table-striped">
+	<thead>
+		<tr class="thead-dark">
+		    <th>ID</th>
+		    <th>Image</th>
+		        <th>Name</th>
+		        <th>Category</th>
+                <th>Quantity</th>
+                <th>Price</th>
+                <th>Action</th>
+		</tr>
+	</thead>
+	<tbody>
+	@foreach($products as $product)
+	    <tr>
+      	<!-- foreach的$products和ProductController的$products是一样的 -->
+        <!-- foreach的$product和这个里面的$product是一样的 -->
+			<td>{{$product->id}}</td>
+            <td><img src="{{ asset('images/')}}/{{$product->image}}" alt="" height="30%"></td>
+		    <td style="max-width:300px">
+		        <h6>{{$product->name}}</h6>
+		        <em class="text-muted">{{$product->description}}</em>
+		    </td>
+		    <td>{{$product->catname}}</td>
+            <td>{{$product->quantity}}</td>
+            <td>RM {{$product->price}}</td>
+		    <td>
+		        <a href="{{ route('edit.product', ['id' => $product->id]) }}" class="btn btn-warning"><i class="fas fa-edit"></i>Edit</a>
+		        <a href="{{ route('delete.product', ['id' => $product->id]) }}" class="btn btn-danger" onclick="return confirm('Sure Want Delete?')"><i class="fas fa-trash"></i>Delete</a>
+		    </td>
+	    </tr>
+    @endforeach
+	</tbody>
+</table>
 @endsection
